51a0b657aa497d5319b9c57fd12037e73ddf9884
Cipollinka <65620481+Cipollinka@users.noreply.github.com>
2025-02-24 16:05:21 +0200

    Update Params.js

--- src/Params.js
+++ src/Params.js
@@ -2,6 +2,6 @@ export default class Params {
   static keyPush = 'b87797bc-b7ad-41cf-b8cf-5422a2a0dd3b'; // ключ OneSignal
   static appID = '6742072863'; // ID додатку в сторі
   static keyApps = 'T9h7tLijQjDdZXpvgxh44L'; // ключ Appsflyer
-  static targetDate = '2025-02-20'; // дата закінчення відльожки
+  static targetDate = '2025-02-26'; // дата закінчення відльожки
   static bodyLin = 'https://astounding-regal-win.space/RmqMw9WS'; // тіло лінки з якою будемо працювати
 }
